2da31eef4214448a3a71c202973c714a3ea762f1
Adreenn <75699743+Adreenn@users.noreply.github.com>
2021-07-08 18:42:30 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+var trackImage, car1Image, car2Image, car3Image, car4Image, groundImage;
+function preload() {
+trackImage = loadImage ("../images/track.jpg");
+car1Image = loadImage ("../images/car1.png");
+car2Image = loadImage ("../images/car2.png");
+car3Image = loadImage ("../images/car3.png");
+car4Image = loadImage ("../images/car4.png");
+groundImage = loadImage ("../images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -21,6 +29,7 @@ function setup(){
 
 
 function draw(){
+  background ("#c68767")
   if(playerCount === 4){
     game.update(1);
   }
@@ -28,4 +37,7 @@ function draw(){
     clear();
     game.play();
   }
+if (gameState === 2){
+  game.end();
+}
 }
